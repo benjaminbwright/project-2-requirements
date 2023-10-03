f82fe033d98ec9d23c1ca7c0a0c234bc95e03e8f
max ohsawa <max.ohsawa@gmail.com>
2023-10-03 14:33:20 -0400

    adds sequelize mysql configuration

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 require("dotenv").config();
 const express = require("express");
+const sequelize = require("./config/connection");
 const exphbs = require("express-handlebars");
 
 const router = require("./routes");
@@ -14,6 +15,9 @@ app.set("view engine", "handlebars");
 
 app.use("/", router);
 
-app.listen(PORT, () => {
-  console.log(`Express server is running on port ${PORT}`);
+sequelize.sync({ force: true }).then(() => {
+  console.log(`Sequelize-MySQL connection established`);
+  app.listen(PORT, () => {
+    console.log(`Express server is running on port ${PORT}`);
+  });
 });
